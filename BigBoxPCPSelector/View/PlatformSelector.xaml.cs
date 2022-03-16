1d1463da42bcd89c60044ce5477b741c47b96fb9
Benuno <Benuno@github.com>
2022-03-16 20:18:19 +0100

    Initial code structure by AtomFry. Adjusted carousel wheel control code, added correct handling of playlist/platform entry/exit.

---
+++ BigBoxPCPSelector/View/PlatformSelector.xaml.cs
@@ -0,0 +1,180 @@
+﻿using BigBoxPCPSelector.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Unbroken.LaunchBox.Plugins;
+using Unbroken.LaunchBox.Plugins.Data;
+
+namespace BigBoxPCPSelector.View
+{
+    /// <summary>
+    /// Interaction logic for PlatformSelector.xaml
+    /// </summary>
+    public partial class PlatformSelector : UserControl, IBigBoxThemeElementPlugin
+    {
+        public bool switchPlatform = false;
+        public bool categoryLock = false;
+        public string startName = null;
+        public string lastCategory = null;
+        public bool gameDetails = false;
+        public string key = null;
+
+        public PlatformSelector()
+        {
+            InitializeComponent();
+            DataContext = PlatformSelectorViewModel.Instance();
+            
+        }
+
+        public bool OnEnter()
+        {
+            this.key = "enter";
+            if (this.switchPlatform)
+            {
+                this.switchPlatform = false;
+                PlatformSelectorViewModel.Instance().ShowPlatform(this.startName);
+                return true;
+            }
+            else
+            {                
+                return false;
+
+            }
+        }
+
+        public bool OnEscape()
+        {
+            this.key = "escape";
+            if (this.switchPlatform && this.gameDetails==false) //&& !this.UseType.Equals("False"))
+            {
+                this.switchPlatform = false;
+                PlatformSelectorViewModel.Instance().escapeKey();
+                return false;
+            }
+            else
+            {
+                if (this.switchPlatform == false && this.gameDetails==false)
+                {
+                    this.switchPlatform = true;
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public bool OnDown(bool held)
+        {
+            this.switchPlatform = false;
+            this.key = "down";
+            return false;
+        }
+
+        public bool OnUp(bool held)
+        {
+            this.key = "up";
+            this.switchPlatform = false;
+            return false;
+        }
+        public bool OnLeft(bool held)
+        {
+            this.key = "left";
+            if (this.switchPlatform)
+            {
+                PlatformSelectorViewModel.Instance().CycleLeft();
+                
+                
+                return true;
+            }
+            return false;
+            
+        }
+
+        public bool OnPageDown()
+        {
+            this.key = "pageDown";
+            return false;
+        }
+
+        public bool OnPageUp()
+        {
+            this.key = "pageUp";
+            return false;
+        }
+
+        public bool OnRight(bool held)
+        {
+            if (this.switchPlatform)
+            {
+                PlatformSelectorViewModel.Instance().CycleRight();
+               
+                return true;
+            }
+            return false;
+        }
+
+        public void OnSelectionChanged(FilterType filterType, string filterValue, IPlatform platform, IPlatformCategory category, IPlaylist playlist, IGame game)
+        {
+                if (playlist != null)
+                {
+                    startName = playlist.Name;
+                
+                    IList<IPlaylist> childs = PluginHelper.DataManager.GetAllPlaylists();
+
+                    List<String> names = new List<String>();
+                    foreach (IPlaylist child in childs)
+                    {
+                        names.Add(child.Name);
+                    }
+                    names.Sort();
+
+                    var foundEntry = names.Where(X => X == startName).FirstOrDefault();
+
+                    if (foundEntry != null && names.Count > 1)
+                    {
+                        int entryIndex = names.IndexOf(foundEntry);
+                        List<String> addToEnd = names.GetRange(0, entryIndex);
+                        names.AddRange(addToEnd);
+                        names.RemoveRange(0, entryIndex);
+                        
+                    }
+
+                    PlatformSelectorViewModel.Instance().setActiveList(names);
+                }
+                else
+                {
+                    startName = platform.Name;
+                    IList<IPlatform> childs = PluginHelper.DataManager.GetAllPlatforms();
+
+                    List<String> names = new List<String>();
+                    foreach (IPlatform child in childs)
+                    {
+                        names.Add(child.Name);
+                    }
+                    names.Sort();
+
+                    var foundEntry = names.Where(X => X == startName).FirstOrDefault();
+
+                    if (foundEntry != null && names.Count > 1)
+                    {
+                        int entryIndex = names.IndexOf(foundEntry);
+                    List<String> addToEnd = names.GetRange(0, entryIndex);
+                    names.AddRange(addToEnd);
+                    names.RemoveRange(0, entryIndex);
+                }
+
+                    PlatformSelectorViewModel.Instance().setActiveList(names);
+                }
+        }
+      
+    }
+}
---
+++ BigBoxPCPSelector/ViewModel/PlatformSelectorViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Unbroken.LaunchBox.Plugins;
+using Unbroken.LaunchBox.Plugins.Data;
+
+namespace BigBoxPCPSelector.ViewModel
+{
+
+    public sealed class PlatformSelectorViewModel
+    {
+        public ObservableCollection<String> ActiveList { get; }
+        public bool WheelIsActive { get; }
+        public List<String> originalList { get; set; }
+        private PlatformSelectorViewModel()
+        {
+            ActiveList = new ObservableCollection<String>(new List<String>{""});
+            originalList = new List<string>();
+            WheelIsActive = true;
+        }
+
+        internal void CycleLeft()
+        {
+            if(ActiveList.Count > 1)
+            {
+                String lastPlatform = ActiveList[ActiveList.Count - 1];
+                ActiveList.Insert(0, lastPlatform);
+                ActiveList.RemoveAt(ActiveList.Count - 1);
+            }
+               
+
+        }
+
+        internal void CycleRight()
+        {
+            if (ActiveList.Count > 1)
+            {
+                String firstPlatform = ActiveList[0];
+                ActiveList.Add(firstPlatform);
+                ActiveList.RemoveAt(0);
+            }
+
+        }
+
+        internal void ShowPlatform(String startName)
+        {
+            String firstPlatform = ActiveList[0];
+            if (ActiveList.Count > 0)// && !startName.Equals(firstPlatform))
+            { 
+                PluginHelper.BigBoxMainViewModel.ShowGames(FilterType.PlatformOrCategoryOrPlaylist, firstPlatform);
+            }
+                
+        }
+
+        //internal void setWheelActive(bool isActive)
+        //{
+        //    WheelIsActive = isActive;
+        //}
+
+        internal void setActiveList(List<String> names)
+        {
+            ActiveList.Clear();
+            foreach (string name in names)
+            {
+                ActiveList.Add(name);
+            }   
+            originalList = names;
+        }
+
+        internal void escapeKey()
+        {   
+            if(originalList.Count > 0)
+            {
+                if (!PluginHelper.StateManager.GetSelectedPlatform().Name.Equals(originalList[0]))
+                {
+                    PluginHelper.BigBoxMainViewModel.ShowGames(FilterType.PlatformOrCategoryOrPlaylist, originalList[0]);
+                }
+                ActiveList.Clear();
+                foreach (string name in originalList)
+                {
+                    ActiveList.Add(name);
+                }                
+            }
+        }
+
+        //single instance across all views.
+        #region singleton implementation 
+        private static PlatformSelectorViewModel _instance;
+
+        public static PlatformSelectorViewModel Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new PlatformSelectorViewModel();
+            }
+            return _instance;
+        }
+        #endregion
+    }
+
+
+}
